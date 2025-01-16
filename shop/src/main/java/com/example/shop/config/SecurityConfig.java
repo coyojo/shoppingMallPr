2d12595a1a45687a7902784ba531e86667f4e29f
YOUNJAE <coyojo@naver.com>
2025-01-17 07:55:05 +0900

    로그인 테스트 코드작성 , 페이지 권한 설정
    로그인 테스트 코드작성 , 페이지 권한을 주기위한 타임리프 설정

--- shop/src/main/java/com/example/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/example/shop/config/SecurityConfig.java
@@ -36,4 +36,5 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
 }
--- shop/src/test/java/com/example/shop/controller/MemberControllerTest.java
+++ shop/src/test/java/com/example/shop/controller/MemberControllerTest.java
@@ -4,17 +4,21 @@ import com.example.shop.dto.MemberFormDto;
 import com.example.shop.entity.Member;
 import com.example.shop.service.MemberService;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc  //MockMvc 테스트를 위해 @AutoConfigureMockMvc 어노테이션
 @Transactional
 @TestPropertySource(locations = "classpath:application-test.properties")
 class MemberControllerTest {
@@ -37,5 +41,32 @@ class MemberControllerTest {
         return memberService.saveMember(member);
     }
 
-    
+    @Test
+    @DisplayName("로그인 성공 테스트")
+    public void loginSuccessTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email,password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated()); //로그인에 성공하여 인증된 상태인지 확인
+
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    public void loginFailTest() throws Exception{
+        String email = "test@email.com";
+        String password = "1234";
+        this.createMember(email,password);
+        mockMvc.perform(formLogin().userParameter("email")
+                .loginProcessingUrl("/members/login")
+                .user(email).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+            // 인증되지 않은 상태를 기대하는 메서드로, 테스트 실패시 테스트가 성공하도록 설정
+
+
+    }
+
 }
